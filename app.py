3d665d12ae4bdd9dd6b22d74098bd96d865ba31f
Kenjiijohn <kenjijohn.sondia@wvsu.edu.ph>
2024-05-22 14:20:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -292,5 +292,7 @@ def about_asd():
 def paper():
     return render_template('paper.html')
 
-
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))  # Use PORT from environment or default to 5000
+    app.run(host='0.0.0.0', port=port)
     
